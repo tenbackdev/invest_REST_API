f9a6eb94da6e3b9f50a4ba87fe582ac8164d9354
tarheel300 <andypguice@gmail.com>
2024-06-15 00:32:44 -0400

    First 4 endpoints

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import tickerRoutes from './routes/tickers.js'
 
 const app = express();
 const PORT = 5000
 
 app.use(bodyParser.json());
+
+app.use('/tickers', tickerRoutes);
+
+app.get('/', (req, res) =>{
+    console.log('My First Get Route');
+    res.send('Hello World!');   
+})
+
+//Endpoints to be added
+    //GET /tickers - finds all ticker symbols
+    //POST /tickers - creates a ticker symbol
+    //GET /tickers/:id -gets a specific ticker symbol
+    //DELETE /tickers/:id - deletes a specific ticker symbol
+    //PATCH /tickers/:id - updates a specific ticker symbol
+//Tutorial: https://www.freecodecamp.org/news/create-crud-api-project/
+//Tutorial walks through the same endpoints for users of a different type of use case
+
+
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
-
-
-
 
 
 
---
+++ routes/tickers.js
@@ -0,0 +1,47 @@
+import express from 'express';
+const router = express.Router();
+
+// Mock DB to be later be connected to a true DB
+let tickers = [
+    {
+        ticker: 'AAPL',
+        ticker_name: 'Apple Computer'
+    },
+    {
+        ticker: 'HD',
+        ticker_name: 'The Home Depot'
+    },
+    {
+        ticker: 'O',
+        ticker_name: 'Realty Income'
+    },
+    {
+        ticker: 'SCHD',
+        ticker_name: 'Schwab High Dividend Yield ETF'
+    }
+]
+
+
+router.get('/', (req, res) => {
+    res.send(tickers);
+})
+
+router.get('/:tickerId', (req, res) => {
+    const {tickerId} = req.params;
+    const foundTicker = tickers.find((ticker) => ticker.ticker === tickerId)
+    res.send(foundTicker);
+})
+
+router.post('/', (req, res) => {
+    const ticker = req.body;
+    tickers.push({...ticker});
+    res.send(`${ticker.ticker} has been added to the database.`);
+})
+
+router.delete('/:tickerId', (req, res) => {
+    const {tickerId} = req.params;
+    tickers = tickers.filter((ticker) => ticker.ticker !== tickerId)
+    res.send(`${tickerId} has been deleted.`);
+})
+
+export default router
