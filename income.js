662c30dce79fef9a4b17787629f35e2f9cb01eb5
tarheel300 <andypguice@gmail.com>
2024-06-23 01:00:08 -0400

    Adding Transaction Route with Type Endpoint

--- income.js
+++ income.js
@@ -4,12 +4,12 @@ import {getHistoricalIncome} from './dbOperations.js';
 
 router.get('/:days?', async (req, res) => {
     try {
-        const histDays = req.params.days || 5000;
+        const histDays = req.params.days || 10000;
         const result = await getHistoricalIncome(histDays);
 
         if (result && result[0]) {
             const incomeRes = result[0].map(row => ({
-                income_dt : row.trans_dt,
+                income_date : row.trans_dt,
                 institution_name : row.inst_nm,
                 account_name : row.acct_nm,
                 ticker : row.ticker,
@@ -19,6 +19,29 @@ router.get('/:days?', async (req, res) => {
                 income_reinvested: row.is_reinvested_flg
             }))
             res.json(incomeRes);
+        } else {
+            throw new Error('Not a result structure');
+        }
+    } catch (err) {
+        console.error('Error querying the database:', err);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
+router.get('/next', async (req, res) => {
+    try {
+        const result = await getAccounts();
+
+        if (result && result[0]) {
+            const accountsRes = result[0].map(row => ({
+                account_id : row.acct_id,
+                account_name: row.acct_nm,
+                account_number: row.acct_nbr.includes('-')
+                                    ? row.acct_nbr.slice(-4)
+                                    : row.acct_nbr,
+                institution_name: row.inst_nm
+            }))
+            res.json(accountsRes);
         } else {
             throw new Error('Not a result structure');
         }
