1acd4cfad04ce91a6bb3e9da18ccc663cbd833f6
tarheel300 <andypguice@gmail.com>
2024-07-02 23:02:07 -0400

    Adding Estimated Income Endpoint

--- income.js
+++ income.js
@@ -2,6 +2,31 @@ import express from 'express';
 const router = express.Router();
 import {getEstimatedIncome, getHistoricalIncome} from './dbOperations.js';
 
+router.get('/estimated', async (req, res) => {
+    try {
+        const result = await getEstimatedIncome();    
+        //note for later, figure out how to move the timezone offset somewhere DRY
+        const rightNow = new Date();     
+        if (result && result[0]) {
+            const estIncRes = result[0].map(row => ({
+                pay_date : row.pay_dt,
+                pay_year : row.trans_yr,
+                account_name : row.acct_nm,
+                ticker : row.ticker,
+                ticker_name : row.ticker_nm,
+                income_announced : row.inc_status === 'Announced' ? true : false,
+                income_amount : row.income_amt,
+            }))
+
+            res.json(estIncRes);
+        } else {
+            throw new Error('Not a result structure');
+        }
+    } catch (err) {
+        console.error('Error querying the database:', err);
+        res.status(500).send('Internal Server Error');
+    }
+})
 
 router.get('/historical/:days?', async (req, res) => {
     try {
@@ -29,17 +54,6 @@ router.get('/historical/:days?', async (req, res) => {
     }
 })
 
-function isSameDay(date1, date2) {
-    const d1 = new Date(date1);
-    const d2 = new Date(date2);
-
-    return (
-        d1.getUTCFullYear() === d2.getUTCFullYear() &&
-        d1.getUTCMonth() === d2.getUTCMonth() &&
-        d1.getUTCDate() === d2.getUTCDate()
-    ); 
-}
-
 router.get('/next', async (req, res) => {
     try {
         const result = await getEstimatedIncome();    
@@ -62,6 +76,17 @@ router.get('/next', async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 })
+
+function isSameDay(date1, date2) {
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+
+    return (
+        d1.getUTCFullYear() === d2.getUTCFullYear() &&
+        d1.getUTCMonth() === d2.getUTCMonth() &&
+        d1.getUTCDate() === d2.getUTCDate()
+    ); 
+}
 
 export default router
 
